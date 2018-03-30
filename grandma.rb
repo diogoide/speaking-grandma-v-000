3d00378ea240d1af6ba9dac3e5b1e43d88b2323d
Diogo Ide <diogoide@gmail.com>
2018-03-30 00:24:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
+  phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
+  phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  else phrase != phrase.upcase
+  phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
   end
 end
